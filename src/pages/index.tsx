a037ecde4b79225f97df94800c7f79a562054f1e
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2022-08-12 16:43:02 +0200

    feat: initial react setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Scene from "@/components/scenes/scene"
+
+const Home = () => {
+	return (
+		<div style={{ position: "absolute", width: "100%", height: "100%", margin: 0, padding: 0, top: 0, left: 0 }}>
+			{<Scene />}
+		</div>
+	)
+}
+
+export default Home
