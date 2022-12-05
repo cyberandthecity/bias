c8f8fc82921c0586a7015907ebf60f5730a3b2fd
Vanessa-Ts <vanessa.tsingunidis@web.de>
2022-12-05 13:46:38 +0100

    Add basics for hint - still in progress

--- src/data/hintPrompt.ts
+++ src/data/hintPrompt.ts
@@ -1,47 +1,46 @@
 import { Message, MessageType } from "@/components/message/message"
 
-export interface HelpPrompt {
-	prompt: Message[]
+export interface HintPrompt {
+	hint: Message[]
 }
 
-export const levelText: HelpPrompt[] = [
+export const levelText: HintPrompt[] = [
 	{
         // Level 1
-		prompt: [
+		hint: [
 			{
 				id: "1",
 				author: "Der Tipp",
 				text: "Versuche auf die Verteilung der Geschlechter zu achten.",
 				delay: 0,
 				decay: 0,
-				type: MessageType.Normal,
-                // TODO: Change MessageType + Add a new messageType tp message.tsx
+				type: MessageType.Hint,
 			},			
 		],
     },
     {
         // Level 2
-		prompt: [
+		hint: [
 			{
 				id: "2",
 				author: "Der Tipp",
 				text: "Versuche darauf zu achten ob in deinem gewählten Datensatz auch Menschen mit Sehhilfen vorkommen.",
 				delay: 0,
 				decay: 0,
-				type: MessageType.Normal,
+				type: MessageType.Hint,
 			},			
 		],
     },
     {
         // Level 3
-		prompt: [
+		hint: [
 			{
 				id: "3",
 				author: "Der Tipp",
 				text: "Auch Eigenschaften die für uns keine Rolle für die Zuordnung von Studenten spielen, können von der KI als Kriterium erkannt werden. Versuche auf die Hintergründe zu achten.",
 				delay: 0,
 				decay: 0,
-				type: MessageType.Normal,
+				type: MessageType.Hint,
 			},			
 		],
     }       
