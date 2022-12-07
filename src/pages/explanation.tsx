27f60630ea708eaa75d21de2c2cf0712dd25fe40
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2022-12-07 18:37:48 +0100

    fix: changed naming

--- src/pages/explanation.tsx
+++ src/pages/explanation.tsx
@@ -1,4 +1,5 @@
 import Background from "@/components/background/background"
+import ClubScene from "@/components/scenes/clubScene"
 import Title from "@/components/title/title"
 import { FunctionComponent } from "react"
 
@@ -8,6 +9,7 @@ const Explanation: FunctionComponent<ExplanationProps> = ({}) => {
 	return (
 		<Background offset={800}>
 			<Title title="Club Entrance" />
+			{/* <ClubScene /> */}
 		</Background>
 	)
 }
