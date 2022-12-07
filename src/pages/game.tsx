916c818dbed19d17ed2b3aead0bb205334ec7a1e
Katja <katja-koerner@gmx.net>
2022-12-07 15:42:26 +0100

    adding basic layout for progress bar

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,6 +1,7 @@
 import AI from "@/components/ai/ai"
 import HINT from "@/components/hint/hint"
 import Background from "@/components/background/background"
+import ProgressBar from "@/components/progressBar/progressBar"
 import DatasetSelector from "@/components/datasetSelector/datasetSelector"
 import { Message } from "@/components/message/message"
 import Title from "@/components/title/title"
@@ -12,6 +13,7 @@ import { FunctionComponent, useEffect, useState } from "react"
 interface GameProps {}
 
 const Game: FunctionComponent<GameProps> = ({}) => {
+	const pageNumber = 2
 	const level = useGame((state) => state.levels[state.currentLevel])
 	const datasets = useGame((state) => state.actions.datasetsForLevel(level))
 	const progressLevel = useGame((state) => state.actions.progressLevel)
@@ -39,6 +41,7 @@ const Game: FunctionComponent<GameProps> = ({}) => {
 			>
 				<AI messages={messages} />
 				<HINT messages={messages} />
+				<ProgressBar percentage={pageNumber}/> 
 				<DatasetSelector
 					title={level.title}
 					datasets={datasets}
