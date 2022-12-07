3d748cddfcaf5ecdfbba67d0393f1507f6235d58
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2022-12-07 18:24:32 +0100

    feat: added linear background colors to theme

--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -3,3 +3,8 @@ export const BackgroundColor = "#F6F8FF"
 
 export const WrongColor = "#DB0000"
 export const CorrectColor = "#57C362"
+
+export const AILightGradient =
+	"linear-gradient(97.73deg, rgba(233, 236, 244, 0.3) 0%, #DDE2F3 100%), linear-gradient(180deg, #F4F1F4 0%, #F0E8F1 100%)"
+export const AIDarkGradient =
+	" linear-gradient(180deg, rgba(244, 241, 244, 0) 0%, #5C237C 100%), linear-gradient(97.73deg, #000000 0%, #1C064D 0.01%)"
