a5cd5560c23d2a462a89e2207ccb1ea2b0ee5779
Moritz Kniebel <moritz.kniebel@student.uni-tuebingen.de>
2022-12-10 15:42:26 +0100

    feat: last screen added

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Door from "./pages/door"
 import Game from "./pages/game"
+import Zoom from "./pages/zoom"
+import ImageGrid from "./pages/zoom3"
 
 const App = () => {
 	return (
@@ -9,6 +11,8 @@ const App = () => {
 				<Route path="/" element={<Door />} />
 				<Route path="/door" element={<Door />} />
 				<Route path="/game" element={<Game />} />
+				<Route path="/zoom" element={<Zoom />} /> //Images are added randomly on page
+				<Route path="/zoom3" element={<ImageGrid />} /> //Huge grid that zooms out over time
 			</Routes>
 		</>
 	)
