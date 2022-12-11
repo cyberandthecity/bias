3730d73e39c6adcdba6e5b92aedac99d6483f817
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2022-12-11 09:27:17 +0100

    feat: added fullscreen modus with rotation

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Route, Routes } from "react-router-dom"
 import Entrance from "./pages/entrance"
 import Explanation from "./pages/explanation"
@@ -5,18 +6,51 @@ import Game from "./pages/game"
 import Zoom from "./pages/zoom"
 import ImageGrid from "./pages/zoom3"
 
-const onDisplay: boolean = false
-
 const App = () => {
-	const scale = onDisplay ? 1.0 : 0.35
-	const rotate = onDisplay ? -90 : 0
-	const translate = onDisplay ? { x: -2160, y: 0 } : { x: 0, y: 0 }
+	const [isFullscreen, setIsFullscreen] = useState(false)
+
+	const activateFullscreen = () => {
+		// Use the browser's full-screen API to make the component full-screen
+		const element = document.documentElement
+
+		if (element.requestFullscreen) {
+			console.log("requestFullscreen")
+			element.requestFullscreen()
+			setIsFullscreen(true)
+		}
+	}
+
+	const scale = isFullscreen ? 1.0 : 0.5
+	const rotate = isFullscreen ? -90 : 0
+	const translate = isFullscreen ? { x: -2160, y: 0 } : { x: 0, y: 0 }
 
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<Entrance scale={scale} rotate={rotate} translate={translate} />} />
-				<Route path="/entrance" element={<Entrance scale={scale} rotate={rotate} translate={translate} />} />
+				<Route
+					path="/"
+					element={
+						<Entrance
+							scale={scale}
+							rotate={rotate}
+							translate={translate}
+							isFullscreen={isFullscreen}
+							activateFullscreen={() => activateFullscreen()}
+						/>
+					}
+				/>
+				<Route
+					path="/entrance"
+					element={
+						<Entrance
+							scale={scale}
+							rotate={rotate}
+							translate={translate}
+							isFullscreen={isFullscreen}
+							activateFullscreen={() => activateFullscreen()}
+						/>
+					}
+				/>
 				<Route path="/explanation" element={<Explanation scale={scale} rotate={rotate} translate={translate} />} />
 				<Route path="/game" element={<Game scale={scale} rotate={rotate} translate={translate} />} />
 				<Route path="/zoom" element={<Zoom />} /> //Images are added randomly on page
