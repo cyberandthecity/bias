75968568e4da5710a59490a1aa1ab4ed23521017
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2022-12-11 09:27:17 +0100

    feat: added fullscreen modus with rotation

--- src/pages/entrance.tsx
+++ src/pages/entrance.tsx
@@ -10,9 +10,17 @@ interface EntranceProps {
 	scale?: number
 	rotate?: number
 	translate?: { x: number; y: number }
+	isFullscreen?: boolean
+	activateFullscreen: () => void
 }
 
-const Entrance: FunctionComponent<EntranceProps> = ({ scale = 1.0, rotate = 0.0, translate = { x: 0, y: 0 } }) => {
+const Entrance: FunctionComponent<EntranceProps> = ({
+	activateFullscreen,
+	isFullscreen = false,
+	scale = 1.0,
+	rotate = 0.0,
+	translate = { x: 0, y: 0 },
+}) => {
 	const messages = useGame((state) => state.entranceInfo.aiMessages)
 
 	let navigate = useNavigate()
@@ -21,75 +29,96 @@ const Entrance: FunctionComponent<EntranceProps> = ({ scale = 1.0, rotate = 0.0,
 		navigate("/explanation")
 	}
 	return (
-		<div
-			style={{
-				position: "absolute",
-				width: "100%",
-				height: "100%",
-				transform:
-					"scale(" + scale + ") rotate(" + rotate + "deg) translate(" + translate.x + "px, " + translate.y + "px)",
-				transformOrigin: "left top",
-			}}
-		>
-			<AI messages={messages} position={{ x: 880, y: 1330 }} scale={1.0} chatOffset={{ x: 500, y: 260 }} />
-
-			<div
-				className="shine"
-				style={{
-					position: "absolute",
-					background: InterfaceColor,
-					top: "1760px",
-					left: "1100px",
-					height: "200px",
-					width: "200px",
-					padding: "18px 30px 8px 30px",
-					borderRadius: "20px",
-					fontSize: "32px",
-					fontWeight: 500,
-					color: "white",
-					boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.16)",
-					cursor: "pointer",
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					zIndex: 1,
-				}}
-				onClick={() => progessToNextScreen()}
-			>
-				Helfen
-			</div>
-
-			<Door />
-
-			<div
-				style={{
-					top: "200px",
-					left: "100px",
-					position: "absolute",
-					width: "2000px",
-					height: "1600px",
-
-					overflow: "hidden",
-					display: "flex",
-					justifyContent: "center",
-
-					background: "rgba(0, 0, 0, 1.0)",
-					zIndex: -2,
-				}}
-			>
+		<>
+			{!isFullscreen && (
 				<div
 					style={{
-						width: "100%",
-						height: "100%",
-						background: "rgba(0, 0, 0, 0.5)",
-						boxShadow: "inset 0px 0px 250px rgba(255, 255, 255, 0.8)",
+						transformOrigin: "left top",
+
+						width: "2160px",
+						height: "50px",
+						background: "red",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						transform: "scale(" + scale + ") ",
+					}}
+					onClick={() => activateFullscreen()}
+				>
+					{" "}
+					Click here to go fullscreen
+				</div>
+			)}
+			<div
+				style={{
+					position: "absolute",
+					width: "100%",
+					height: "100%",
+					transform:
+						"scale(" + scale + ") rotate(" + rotate + "deg) translate(" + translate.x + "px, " + translate.y + "px)",
+					transformOrigin: "left top",
+				}}
+			>
+				<AI messages={messages} position={{ x: 880, y: 1330 }} scale={1.0} chatOffset={{ x: 500, y: 260 }} />
+
+				<div
+					className="shine"
+					style={{
 						position: "absolute",
+						background: InterfaceColor,
+						top: "1760px",
+						left: "1100px",
+						height: "200px",
+						width: "200px",
+						padding: "18px 30px 8px 30px",
+						borderRadius: "20px",
+						fontSize: "32px",
+						fontWeight: 500,
+						color: "white",
+						boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.16)",
+						cursor: "pointer",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
 						zIndex: 1,
 					}}
-				/>
-				<video autoPlay loop muted playsInline src="/videos/party.mp4" style={{ filter: "blur(20px)" }} />
+					onClick={() => progessToNextScreen()}
+				>
+					Helfen
+				</div>
+
+				<Door />
+
+				<div
+					style={{
+						top: "200px",
+						left: "100px",
+						position: "absolute",
+						width: "2000px",
+						height: "1600px",
+
+						overflow: "hidden",
+						display: "flex",
+						justifyContent: "center",
+
+						background: "rgba(0, 0, 0, 1.0)",
+						zIndex: -2,
+					}}
+				>
+					<div
+						style={{
+							width: "100%",
+							height: "100%",
+							background: "rgba(0, 0, 0, 0.5)",
+							boxShadow: "inset 0px 0px 250px rgba(255, 255, 255, 0.8)",
+							position: "absolute",
+							zIndex: 1,
+						}}
+					/>
+					<video autoPlay loop muted playsInline src="/videos/party.mp4" style={{ filter: "blur(20px)" }} />
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
