42b1f4440dee65eb5cce9ad5e0ecab420cfc0ee0
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2022-12-12 15:33:16 +0100

    feat: removed animation

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,8 +1,7 @@
+import "@/styles/message.css"
 import { InterfaceColor } from "@/utils/theme"
 import { FunctionComponent } from "react"
 import { ChatOrientation } from "../chat/chat"
-import { motion, AnimatePresence } from "framer-motion"
-import "@/styles/message.css"
 
 export enum MessageType {
 	Normal,
@@ -75,37 +74,34 @@ const ChatMessage: FunctionComponent<MessageProps> = ({ id, author, text, type =
 	}
 
 	return (
-		<motion.div key={id} initial="initial" animate="enter" variants={variants} layout>
-			<div
-				className={"tailBreakdown"}
-				style={{
-					position: "relative",
-					background: backgroundColor(type),
-					color: color(type),
-					boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.14)",
+		<div
+			style={{
+				position: "relative",
+				background: backgroundColor(type),
+				color: color(type),
+				boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.14)",
 
-					padding: "20px 30px 20px 30px",
-					width: "fit-content",
-					fontSize: "32px",
-					fontWeight: 500,
-					borderTopLeftRadius: "40px 38px",
-					borderTopRightRadius: "40px 38px",
-					borderBottomLeftRadius: "40px 38px",
-					borderBottomRightRadius: "40px 38px",
-					//backdropFilter: "blur(20px)",
+				padding: "20px 30px 20px 30px",
+				width: "fit-content",
+				fontSize: "32px",
+				fontWeight: 500,
+				borderTopLeftRadius: "40px 38px",
+				borderTopRightRadius: "40px 38px",
+				borderBottomLeftRadius: "40px 38px",
+				borderBottomRightRadius: "40px 38px",
+				//backdropFilter: "blur(20px)",
+			}}
+		>
+			<pre
+				style={{
+					color: "white",
+					opacity: 0.5,
 				}}
 			>
-				<pre
-					style={{
-						color: "white",
-						opacity: 0.5,
-					}}
-				>
-					{author}
-				</pre>
-				{text}
-			</div>
-		</motion.div>
+				{author}
+			</pre>
+			{text}
+		</div>
 	)
 }
 
