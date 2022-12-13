35dc956443ab2447df6274f0190ae1bf1408918d
Moritz Kniebel <moritz.kniebel@student.uni-tuebingen.de>
2022-12-13 11:48:18 +0100

    feat: latest version of imageadder

--- src/pages/zoom.tsx
+++ src/pages/zoom.tsx
@@ -1,9 +1,6 @@
 import React from "react"
 import ImageAdder2 from "@/components/imageadder2/imageadder2"
-import AI from "@/components/ai/ai"
-import HINT from "@/components/hint/hint"
 import Background from "@/components/background/background"
-import { Message } from "@/components/message/message"
 import Title from "@/components/title/title"
 import { Dataset, useGame } from "@/stores/gameStore"
 import { FunctionComponent, useEffect, useState } from "react"
@@ -22,36 +19,30 @@ const Zoom: FunctionComponent<ZoomProps> = ({
 	toggleFullscreen,
 }) => {
 	const level = useGame((state) => state.levels[state.currentLevel])
-	const datasets = useGame((state) => state.actions.datasetsForLevel(level))
-	const progressLevel = useGame((state) => state.actions.progressLevel)
 
-	const [isChecking, setIsChecking] = useState(false)
-	const [messages, setMessages] = useState<Message[]>(level.aiPrompt.prompt)
-	// Generate an array of random image URLs
-	const imageUrls = Array.from(
-		{ length: 10000 },
-		() => `/datasets/tech_5x4/set01/image_${Math.floor(Math.random() * 7)}.png`
-	)
-
-	useEffect(() => {
-		setIsChecking(false)
-		setMessages(level.aiPrompt.prompt)
-	}, [level])
+	
 	return (
 		<Background offset={800} scale={scale} rotate={rotate} translate={translate}>
 			<Title title="Bias & KI" />
 			<div
 				style={{
+					top: "200px",
+					left: "100px",
 					position: "absolute",
+					width: "2000px",
+					height: "1600px",
+
+					overflow: "hidden",
 					display: "flex",
-					flexDirection: "column",
-					width: "100%",
-					height: "100%",
 					justifyContent: "center",
+					
+					//display: "flex",
+					//flexDirection: "column",
+					//width: "100%",
+					//height: "100%",
+					//justifyContent: "center",
 				}}
 			>
-				<AI messages={messages} position={{ x: 0, y: 0 }} />
-				<HINT messages={messages} />
 				<ImageAdder2 />
 			</div>
 		</Background>
