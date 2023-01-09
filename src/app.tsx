ab54efc8beb0264389ee8e08c314a304256dd421
Jan-Niklas Dihlmann <j.dihlmann@googlemail.com>
2023-01-09 19:53:42 +0100

    feat: game auto reset after five minutes

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,54 @@
-import { useState } from "react"
-import { Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Route, Routes, useNavigate } from "react-router-dom"
 import AICanvas from "./components/ai/aiCanvas"
 import Entrance from "./pages/entrance"
 import Explanation from "./pages/explanation"
 import Game from "./pages/game"
 import Zoom from "./pages/zoom"
 import ImageGrid from "./pages/zoom3"
+import { useGame } from "./stores/gameStore"
 
 const App = () => {
+	let navigate = useNavigate()
+	const resetLevel = useGame((state) => state.actions.resetLevel)
+
+	let didInputOccur = false
 	const [isFullscreen, setIsFullscreen] = useState(false)
 
 	const scale = isFullscreen ? 0.33333 : 0.5
 	const rotate = isFullscreen ? -90 : 0
 	const translate = isFullscreen ? { x: -2160, y: 0 } : { x: 0, y: 0 }
+
+	const resetGame = () => {
+		resetLevel()
+		navigate("/")
+	}
+
+	const onInput = () => {
+		didInputOccur = true
+	}
+
+	useEffect(() => {
+		document.addEventListener("click", onInput)
+		document.addEventListener("touchstart", onInput)
+
+		return () => {
+			document.removeEventListener("click", onInput)
+			document.removeEventListener("touchstart", onInput)
+		}
+	}, [])
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (!didInputOccur) {
+				resetGame()
+			} else {
+				didInputOccur = false
+			}
+		}, 60000 * 5) // check for input every 5 minutes
+
+		return () => clearInterval(interval)
+	}, [])
 
 	return (
 		<>
